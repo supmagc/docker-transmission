5f378f08c26080b1ac3e3b21603f832de060477c
supmagc <supmagc@gmail.com>
2017-12-22 17:12:40 +0100

    Cleanup and cron fixes

--- root/crons/cleanup.py
+++ root/crons/cleanup.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 import common
 
 client = common.getTransmissionClient('cleanup')
-client.blocklist_update()
+print('Running cleanup')
 torrents = client.get_torrents()
 for torrent in torrents:
     if torrent.isFinished and not torrent.error and datetime.fromtimestamp(torrent.doneDate) + timedelta(days=1) < datetime.now():
